baa0e98f0d5ca7f78b96f50ac0f5b961ed35798d
codershubh1432 <167545010+codershubh1432@users.noreply.github.com>
2026-03-07 15:35:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -101,6 +101,16 @@ app.get("/", (req, res) => {
   res.redirect("/listings");
 });
 
+app.get("/listings/:id/confirm", async (req, res) => {
+
+  const { id } = req.params;
+
+  const listing = await Listing.findById(id);
+
+  res.render("successpage", { listing });
+
+});
+
 
 
 
